f0f6a32ec8e1f3054b236088749df94ecf25cad8
Nathan Rajlich <nathan@tootallnate.net>
2018-02-20 16:09:04 -0800

    Works with TTY streams like `process.stdin`

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function createLineIterator(stream) {
   let done = false;
   let leftover = '';
   const iterator = createStreamIterator(stream);
+  const delimiter = stream.isTTY ? '\r' : /\r?\n/;
 
   async function next() {
     if (lines.length > 0) {
@@ -12,7 +13,7 @@ module.exports = function createLineIterator(stream) {
     }
     let value;
     ({ done, value } = await iterator.next());
-    lines.push(...(leftover + (value || '')).split('\n'));
+    lines.push(...(leftover + (value || '')).split(delimiter));
     leftover = done ? null : lines.pop();
     //console.log({ lines, leftover });
     return next();
