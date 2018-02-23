fcc3b4adf933cd73c648ad5a776211cb115c77df
Nathan Rajlich <nathan@tootallnate.net>
2018-02-22 19:01:03 -0800

    Remove Ctrl+C / Ctrl+D mapping
    Doesn't belong in this repo

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = function createLineIterator(stream) {
   let done = false;
   let leftover = '';
   const iterator = createStreamIterator(stream);
-  const delimiter = stream.isRaw ? '\r' : /\r?\n/;
 
   async function next() {
     if (lines.length > 0) {
@@ -13,19 +12,7 @@ module.exports = function createLineIterator(stream) {
     }
     let value;
     ({ done, value } = await iterator.next());
-
-    if (stream.isRaw && value && value.length === 1) {
-      if (value[0] === 3) {
-        // Map Ctrl+C to SIGINT
-        process.kill(process.pid, 'SIGINT');
-        value = '';
-      } else if (value[0] === 4) {
-        // Map Ctrl+D to the stream's "end" event
-        stream.emit('end');
-        value = '';
-      }
-    }
-
+    const delimiter = stream.isRaw ? '\r' : /\r?\n/;
     lines.push(...(leftover + (value || '')).split(delimiter));
     leftover = done ? null : lines.pop();
     return next();
